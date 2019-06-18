dc24e571f95bde62e692027bc261aaa816c4e452
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-06-18 13:27:33 -0600

    Added first two eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+//Event listener #1 mouseover
+let funBus = document.querySelector('h1')
+console.log(funBus)
+funBus.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = 'yellow';
+})
+
+//Event Listener #2 mouseoff
+funBus.addEventListener('mouseout', event => {
+    event.target.style.backgroundColor = 'white';
+})
