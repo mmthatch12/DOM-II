497bfcb5c865482089d03de554ea7d5a9a273710
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-06-18 15:32:17 -0600

    Added 8th event listener

--- js/index.js
+++ js/index.js
@@ -50,4 +50,18 @@ containHome.addEventListener('click', event => {
 })
 
 
-//Event Listener #8 
+//Event Listener #8 paste
+let whereToPaste = document.querySelector('.intro p')
+whereToPaste.addEventListener('paste', event => {
+    let paste = (event.clipboardData || window.clipboardData).getData('text')
+    paste = paste.toUpperCase();
+
+    const selection = window.getSelection()
+    if (!selection.rangeCount) return false
+    selection.deleteFromDocument()
+    selection.getRangeAt(0).insertNode(document.createTextNode(paste))
+
+    event.preventDefault()
+})
+
+//Event Listener #9
