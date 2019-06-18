056a156b7c5daead8169cf6bc55344e0a408a71d
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-06-18 13:54:09 -0600

    Added 3rd and 4th event listener

--- js/index.js
+++ js/index.js
@@ -4,9 +4,32 @@ let funBus = document.querySelector('h1')
 console.log(funBus)
 funBus.addEventListener('mouseover', event => {
     event.target.style.backgroundColor = 'yellow';
+    event.target.style.color = 'white'
 })
 
 //Event Listener #2 mouseoff
 funBus.addEventListener('mouseout', event => {
     event.target.style.backgroundColor = 'white';
+    event.target.style.color = 'black'
 })
+
+//Event listener #3 dblclick
+let busImg = document.querySelectorAll('.img-content')
+busImg[0].addEventListener('dblclick', event => {
+    event.target.style.border = '5px dashed yellow'
+})
+
+//Event listener #4 wheel
+function zoom(event) {
+    event.preventDefault();
+
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    busImg[1].style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+busImg[1].onwheel = zoom;
+
+//Event listener #5 Keydown
+let 
