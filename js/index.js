da327134abefd1684425762ff55c7c226a96b5c5
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-09 13:41:12 -0600

    finished dblclick event listener

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 // Your code goes here
+
+//click event
 const busHead = document.querySelector('.logo-heading')
 busHead.addEventListener('click', event => event.target.style.color = 'blue')
 
+//mouseenter event
+
+const nava = document.querySelectorAll('.nav a')
+nava.forEach(item => item.addEventListener('mouseenter', event => event.target.style.color = 'blue'))
+
+//mouseleave event
+nava.forEach(item => item.addEventListener('mouseleave', event => event.target.style.color = 'black'))
+
+//keydown event not working yet
+const textcont = document.querySelector('.text-content h2')
+console.log(textcont)
+
+textcont.addEventListener('keydown', event => event.target.style.color = 'blue')
+
+//dblclick event
+const imgg = document.querySelectorAll('img')
+imgg.forEach(item => item.addEventListener('dblclick', event => event.target.style.border = '3px dashed blue'))
+imgg.forEach(item => item.addEventListener('dblclick', event => event.target.style.borderRadius = '70px'))
